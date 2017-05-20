87daf8d5ac8264b7daf83dc130e736f58833a5b3
Galushkin Pavel <gpg@ua.fm>
2017-05-20 14:44:58 +0300

    minor refactoring

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,75 +1,103 @@
 package task2;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
  * Created by Galushkin Pavel on 19.05.2017.
  */
 public class Main {
-    private static int HEAPSIZE;
-    private static int[] H;
-    //private static List<String> rotateList = new ArrayList<>();
+    class BinaryHeap {
+        private final int[] heap;
+        private int heapsize;
 
-    public static void siftDown(int i) {
-        while (true) {
-            int leftChild = 2 * i + 1;
-            int rightChild = 2 * i + 2;
-            int maxIndex = i;
+        public BinaryHeap(int[] heap) {
+            this.heap = heap;
+            this.heapsize = heap.length;
 
-            if (leftChild < HEAPSIZE && H[leftChild] > H[maxIndex])
-                maxIndex = leftChild;
-
-            if (rightChild < HEAPSIZE && H[rightChild] > H[maxIndex])
-                maxIndex = rightChild;
-
-            if (maxIndex == i) return;
-            //rotateList.add(i + " " + maxIndex);
-            int temp = H[i];
-            H[i] = H[maxIndex];
-            H[maxIndex] = temp;
-            i = maxIndex;
+            // Building a heap from an array O(log N)
+            for (int i = heapsize / 2; i >= 0; i--) {
+                siftDown(i);
+            }
         }
-    }
 
-    public static int getMax() {
+        private void siftDown(int i) {
+            while (true) {
+                int leftChild = 2 * i + 1;
+                int rightChild = 2 * i + 2;
+                int maxIndex = i;
 
-        int max = H[0];
-        H[0] = H[HEAPSIZE - 1];
-        HEAPSIZE--;
-        siftDown(0);
-        return max;
-    }
+                if (leftChild < heapsize && heap[leftChild] > heap[maxIndex])
+                    maxIndex = leftChild;
 
-    public static void heapSort() {
-        for (int i = HEAPSIZE - 1; i >= 0; i--) {
-            H[i] = getMax();
+                if (rightChild < heapsize && heap[rightChild] > heap[maxIndex])
+                    maxIndex = rightChild;
+
+                if (maxIndex == i) return;
+
+                int temp = heap[i];
+                heap[i] = heap[maxIndex];
+                heap[maxIndex] = temp;
+                i = maxIndex;
+            }
+        }
+
+        private int getMax() {
+            int max = heap[0];
+            heap[0] = heap[heapsize - 1];
+            heapsize--;
             siftDown(0);
+            return max;
         }
+
+        // sort array without extra memory
+        public int[] heapSort() {
+            for (int i = heapsize - 1; i > 0; i--) {
+                heap[i] = getMax();
+                siftDown(0);
+            }
+            return heap;
+        }
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.println(Arrays.stream(arr)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" ")));
     }
 
     public static void main(String[] args) {
-        HEAPSIZE = 20;
+        int arraySize = 20;
         int low = 0, high = 100;
         int k = 4;
-        H = new Random().ints(HEAPSIZE, low, high).toArray();
-        // Arrays.sort(arr); не используем потому что..
-        // PriorityQueue не используем потому что нет доступа по индексу, и при большом k придется перебирать все значения
-        System.out.println(Arrays.stream(H)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining(" ")));
+        int[] arr = new Random().ints(arraySize, low, high).toArray();
 
-        // make a heap from array
-        for (int i = HEAPSIZE / 2; i >= 0; i--) {
-            siftDown(i);
-        }
-        System.out.println(Arrays.stream(H)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining(" ")));
-        heapSort();
-        System.out.println(Arrays.stream(H)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining(" ")));
-        System.out.println("Answer: array[k] = " + H[k-1]);
+        /**
+         * Sorts the specified array into ascending numerical order.
+         *
+         * <p>Implementation note: The sorting algorithm is a Dual-Pivot Quicksort
+         * by Vladimir Yaroslavskiy, Jon Bentley, and Joshua Bloch. This algorithm
+         * offers O(n log(n)) performance on many data sets that cause other
+         * quicksorts to degrade to quadratic performance, and is typically
+         * faster than traditional (one-pivot) Quicksort implementations.
+         *
+         * @param a the array to be sorted
+         */
+        Arrays.sort(arr);
+        // PriorityQueue doesn't approach since doesn't provide access to elements by index
+
+        // print unsorted array
+        //printArray(arr);
+
+        BinaryHeap binaryHeap = new Main().new BinaryHeap(arr);
+
+        // sort array. O(N log N)
+        arr = binaryHeap.heapSort();
+
+        // print sorted array (for testing result)
+        printArray(arr);
+
+        System.out.println("Answer: array[k] = " + arr[k - 1]);
     }
 }
