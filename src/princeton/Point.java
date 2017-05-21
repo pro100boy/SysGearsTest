fe99c068010b8dfdf0bd406653d14846a171b6e5
Galushkin Pavel <gpg@ua.fm>
2017-05-21 22:44:45 +0300

    Task 4 done

--- src/princeton/Point.java
+++ src/princeton/Point.java
@@ -14,9 +14,12 @@ import edu.princeton.cs.algs4.StdOut;
 public class Point {
     public final int x;
     public final int y;
+    private static int cnt = 0;
+    private int i = 0;
 
     // create and initialize a point with given (x, y)
     public Point(int x, int y) {
+        this.i = cnt++;
         this.x = x;
         this.y = y;
     }
@@ -44,9 +47,9 @@ public class Point {
     public static int ccw(Point a, Point b, Point c) {
         // return a.x*b.y - a.y*b.x + a.y*c.x - a.x*c.y + b.x*c.y - b.y*c.x;
         double area2 = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
-        if      (area2 < 0) return -1;
+        if (area2 < 0) return -1;
         else if (area2 > 0) return +1;
-        else                return  0;
+        else return 0;
     }
 
     // is a-b-c collinear?
@@ -60,12 +63,10 @@ public class Point {
         if (ccw(a, b, c) != 0) return false;
         if (a.x == b.x && a.y == b.y) {
             return a.x == c.x && a.y == c.y;
-        }
-        else if (a.x != b.x) {
+        } else if (a.x != b.x) {
             // ab not vertical
             return (a.x <= c.x && c.x <= b.x) || (a.x >= c.x && c.x >= b.x);
-        }
-        else {
+        } else {
             // ab not horizontal
             return (a.y <= c.y && c.y <= b.y) || (a.y >= c.y && c.y >= b.y);
         }
@@ -74,7 +75,7 @@ public class Point {
 
     // return string representation of this point
     public String toString() {
-        return "(" + x + ", " + y + ")";
+        return String.format("P%d(%d, %d) ", i, x, y);
     }
 
 
