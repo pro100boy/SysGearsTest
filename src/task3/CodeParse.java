5ea9949335be1d3889552a33d1ed52cb99465222
Galushkin Pavel <gpg@ua.fm>
2017-05-22 21:36:38 +0300

    Task 3 init

---
+++ src/task3/CodeParse.java
@@ -0,0 +1,82 @@
+package task3;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Created by User on 22.05.2017.
+ */
+public class CodeParse {
+    private static Pattern ptn = Pattern.compile("^([A-Za-z0-9]{4})[A-Za-z0-9]{0,2}([Rr](d)?(f)?(?:(?<=[df])(?:[0-9]{3})|(?:[0-9]{4}))(?:\\1)?)(?:([+-])(\\d{3}))?(\\d+)[a-zA-Z]{0,2}$");
+
+    /**
+     * Convert an octal value to its corresponding ASCII value.
+     * Convert from octal(base 8)->decimal(base 10)->character.
+     */
+    private static String octToASCII(String octStr) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < octStr.length() - 2; i += 3) {
+            // Convert Octal(base8) to decimal(base 10).
+            int iOctal = Integer.parseInt(octStr.substring(i, i + 3), 8);
+            // Cast decimal to its corresponding ASCII value.
+            char cOctal = (char) iOctal;
+            stringBuilder.append(cOctal);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String parse(String codeStr) {
+        String[] codeParts = new String[7];
+        Matcher mtr = ptn.matcher(codeStr);
+
+        if (mtr.matches()) {
+            codeParts[0] = codeStr.trim();     // code
+            codeParts[1] = mtr.group(1);    // drive code
+            codeParts[2] = mtr.group(2);    // travel paper
+            if (mtr.group(3) != null) {     // danger
+                codeParts[3] = "true";
+            } else {
+                codeParts[3] = "false";
+            }
+            if (mtr.group(4) != null) {     // ragile
+                codeParts[4] = "true";
+            } else {
+                codeParts[4] = "false";
+            }
+            if (mtr.group(5) != null) {     // temperature
+                codeParts[5] = mtr.group(5).concat(Integer.toString(Integer.parseInt(mtr.group(6))));
+            }
+            // name
+            codeParts[6] = octToASCII(mtr.group(7));
+
+            // build result string
+            String res = Arrays.stream(codeParts).map(s -> {
+                if (Objects.isNull(s)) return "";
+                else return "\"" + s + "\"";
+            }).collect(Collectors.joining(","));
+            return res;
+        } else {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(parse("CAZgRf820167151156145"));
+        System.out.println(parse("RMuiRdf010160141151156164"));
+        System.out.println(parse("lims8r3860lims1631411561441"));
+        System.out.println(parse("GZQRyr6870GZQR+0041431501451451631455A"));
+        System.out.println(parse("qkMfPjrd0561411551551651561511641511571567"));
+        System.out.println(parse("EOcTkerf389-0201511431450551431621451411550"));
+
+        //Output:
+        //"CAZgRf820167151156145","CAZg","Rf820","false","true",,"wine"
+        //"RMuiRdf010160141151156164","RMui","Rdf010","true","true",,"paint"
+        //"lims8r3860lims1631411561441","lims","r3860lims","false","false",,"sand"
+        //"GZQRyr6870GZQR+0041431501451451631455A","GZQR","r6870GZQR","false","false","+4","cheese"
+        //"qkMfPjrd0561411551551651561511641511571567","qkMf","rd056","true","false",,"ammunition"
+        //"EOcTkerf389-0201511431450551431621451411550","EOcT","rf389","false","true","-20","ice-cream"
+    }
+}
--- src/task3/Main.java
+++ src/task3/Main.java
@@ -1,4 +1,54 @@
 package task3;
 
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    private final String FILENAME = "./decoded_data.csv";
+
+    private final List<String> inputCodes()
+    {
+        final String EXITCODE = "end";
+        final List<String> codeList = new LinkedList<>();
+
+        while (true)
+        {
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                System.out.println("Please input code: ");
+                String codeStr = reader.readLine();
+                if (codeStr.equals(EXITCODE)) break;
+                else codeList.add(codeStr);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        return codeList;
+    }
+
+    private final StringBuilder decodeData(List<String> codeList)
+    {
+        final String HEADER = "\"шифр\",\"код водителя\",\"код путевого листа\",\"опасный\",\"хрупкий\",\"температура\",\"наименование\"";
+        StringBuilder decodedStrins = new StringBuilder(HEADER);
+
+        codeList.stream().forEach(s -> decodedStrins.append(CodeParse.parse(s)).append(System.lineSeparator()));
+
+        return decodedStrins;
+    }
+
+    private final void save2CSV(StringBuilder decodedStrins){
+        //append string buffer/builder to buffered writer
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME))) {
+            bw.append(decodedStrins);//Internally it does decodedStrins.toString();
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        List<String> list = main.inputCodes();
+    }
 }
