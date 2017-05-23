7868b224f6b1ec649b3004a9773f6c52b0274424
Galushkin Pavel <gpg@ua.fm>
2017-05-23 11:32:03 +0300

    task 3 done

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -7,19 +7,28 @@ import java.util.List;
 public class Main {
     private final String FILENAME = "./decoded_data.csv";
 
-    private final List<String> inputCodes()
-    {
+    private enum Encoding {
+        WIN("cp1251"),
+        UTF("UTF-8");
+        private final String charset;
+
+        Encoding(String charset) {
+           this.charset = charset;
+        }
+    }
+
+    private final List<String> inputCodes() {
         final String EXITCODE = "end";
         final List<String> codeList = new LinkedList<>();
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true)
-        {
-            try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-                System.out.println("Please input code: ");
+        while (true) {
+            System.out.println("Please input code or 'end' to stop input: ");
+            try {
                 String codeStr = reader.readLine();
                 if (codeStr.equals(EXITCODE)) break;
                 else codeList.add(codeStr);
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
@@ -27,19 +36,18 @@ public class Main {
         return codeList;
     }
 
-    private final StringBuilder decodeData(List<String> codeList)
-    {
+    private final StringBuilder decodeData(List<String> codeList) {
         final String HEADER = "\"шифр\",\"код водителя\",\"код путевого листа\",\"опасный\",\"хрупкий\",\"температура\",\"наименование\"";
         StringBuilder decodedStrins = new StringBuilder(HEADER);
 
-        codeList.stream().forEach(s -> decodedStrins.append(CodeParse.parse(s)).append(System.lineSeparator()));
+        codeList.stream().forEach(s -> decodedStrins.append(System.lineSeparator()).append(CodeParse.parse(s)));
 
         return decodedStrins;
     }
 
-    private final void save2CSV(StringBuilder decodedStrins){
+    private final void save2CSV(StringBuilder decodedStrins, Encoding encoding) {
         //append string buffer/builder to buffered writer
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME))) {
+        try (Writer bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FILENAME), encoding.charset))) {
             bw.append(decodedStrins);//Internally it does decodedStrins.toString();
             bw.flush();
         } catch (IOException e) {
@@ -50,5 +58,8 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         List<String> list = main.inputCodes();
+        StringBuilder stringBuilder = main.decodeData(list);
+        main.save2CSV(stringBuilder, Encoding.WIN);
+        System.out.println(stringBuilder.toString());
     }
 }
